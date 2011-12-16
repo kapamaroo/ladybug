9b45bee39c7ce636de7a6d2a978104e124ec9ffb
Maroudas Manolis <kapamaroo@gmail.com>
2011-12-16 22:11:12 +0200

    remove from time being the "final_code.c/h"
    they need lots of work yet

--- final_code.h
+++ final_code.h
@@ -1,6 +1,25 @@
 #ifndef _FINAL_CODE_H
 #define _FINAL_CODE_H
 
-void generate_final_code();
+#include <stdio.h>
+#include <stdlib.h>
+#include "semantics.h"
+#include "build_flags.h"
+#include "ir.h"
+#include "instructionset.h"
+
+typedef struct code_rep{
+    inst_t *node;
+    struct code_rep *next;
+    struct code_rep *begin;
+    int branch; // 0 no branch 1 branch use begin;
+}code_rep;
+
+
+code_rep *global_begin;
+
+
+code_rep *insert_in_code(code_rep *start,inst_t *node);
+int travel_nodes(ir_node_t *start,code_rep *node);
 
 #endif
