a1e6bfece320a4e94a19ccbe617a955b1a1e8584
Maroudas Manolis <kapamaroo@gmail.com>
2011-12-29 21:05:58 +0200

    add statements.c/h
    Now we create at first a high level intermediate representation
    using the struct statement_t. This struct will be translated into
    ir_node_t which is the low level intermediate representation.
    
    This is a first step to implement more sophisticated implementations
    afterwards.
    
    Thanks to Vasilis Vasiliadis for the initial structs.

---
+++ statements.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "statements.h"
+#include "symbol_table.h" //ladybug's symbol table
+
+statement_t *statement_root_module[MAX_NUM_OF_MODULES];
+int statement_root_module_current;
+
+void init_statements() {
+    int i;
+
+    for(i=0; i<MAX_NUM_OF_MODULES; i++) {
+        statement_root_module[i] = NULL;
+    }
+
+    statement_root_module_current = 0;
+}
+
+statement_t *new_statement_t(enum StatementType type) {
+    statement_t *new_statement;
+
+    new_statement = (statement_t*)malloc(sizeof(struct statement_t));
+    new_statement->type = type;
+    new_statement->prev = NULL;
+    new_statement->next = NULL;
+    new_statement->last = new_statement;
+    new_statement->join = NULL;
+    new_statement->return_point = 0;
+
+    return new_statement;
+}
+
+statement_t *statement_if(expr_t *condition, statement_t *_true, statement_t *_false) {
+    statement_t *new_if;
+
+    new_if = new_statement_t(ST_If);
+    new_if->_if.condition = condition;
+    new_if->_if._true = _true;
+    new_if->_if._true = _true;
+
+    //propagate return_point to the last statement
+    if (_true->last->return_point && _false->last->return_point) {
+        new_if->return_point = 1;
+    }
+
+    return new_if;
+}
+
+statement_t *statement_while(expr_t *condition, statement_t *loop) {
+    statement_t *new_while;
+
+    new_while = new_statement_t(ST_While);
+    new_while->_while.condition = condition;
+    new_while->_while.loop = loop;
+
+    return new_while;
+}
+
+statement_t *statement_assignment(var_t *var, expr_t *expr) {
+    statement_t *new_assign;
+
+    new_assign = new_statement_t(ST_Assignment);
+
+    if (expr->expr_is==EXPR_STRING) {
+        new_assign->_assignment.type = AT_String;
+        new_assign->_assignment.string = expr->cstr;
+    } else {
+        new_assign->_assignment.type = AT_Expression;
+        new_assign->_assignment.expr = expr;
+
+        if (var->id_is==ID_RETURN) {
+            new_assign->return_point = 1;
+        }
+    }
+
+    return new_assign;
+}
+
+//statement_t *statement_assignment_str(var_t *var, char *string) {}
+
+statement_t *statement_for(char *id, iter_t *iter_space, statement_t *loop) {
+    statement_t *new_for;
+    sem_t *sem_guard;
+
+    sem_guard = sm_find(id);
+    if ((!sem_guard || sem_guard->id_is!=ID_VAR_GUARDED)) {
+        //bad id for variable
+        return new_statement_t(ST_BadStatement);
+    }
+
+    new_for = new_statement_t(ST_For);
+    new_for->_for.from = iter_space->start;
+    new_for->_for.to = iter_space->stop;
+    new_for->_for.var = sem_guard->var;
+    new_for->_for.loop = loop;
+
+    if (iter_space->step->ival==1) {
+        new_for->_for.type = FT_To;
+    } else {
+        new_for->_for.type = FT_DownTo;
+    }
+
+    return new_for;
+}
+
+statement_t *statement_call(char *id, expr_list_t *expr_params) {
+    statement_t *new_call;
+    sem_t *sem_sub;
+
+    sem_sub = sm_find(id);
+
+    if (!sem_sub || (sem_sub->id_is != ID_PROC &&
+                     sem_sub->id_is != ID_FORWARDED_PROC &&
+                     sem_sub->id_is != ID_FUNC &&
+                     sem_sub->id_is != ID_FORWARDED_FUNC)) {
+        //bad call
+        return new_statement_t(ST_BadStatement);
+    }
+
+    new_call = new_statement_t(ST_Call);
+    new_call->_call.subprogram = sem_sub->subprogram;
+    new_call->_call.expr_params = expr_params;
+
+    return new_call;
+}
+
+statement_t *statement_with(var_t *var, statement_t *statement) {
+    statement_t *new_with;
+
+    new_with = new_statement_t(ST_With);
+    new_with->_with.var = var;
+    new_with->_with.statement = statement;
+
+    return new_with;
+}
+
+statement_t *statement_read(var_list_t *var_list) {
+    statement_t *new_read;
+
+    new_read = new_statement_t(ST_Read);
+    new_read->_read.var_list = var_list;
+
+    return new_read;
+}
+
+statement_t *statement_write(expr_list_t *expr_list) {
+    statement_t *new_write;
+
+    new_write = new_statement_t(ST_Write);
+    new_write->_write.expr_list = expr_list;
+
+    return new_write;
+}
+
+statement_t *link_statements(statement_t *child, statement_t *parent) {
+    //return the head of the linked list
+    if (parent && child) {
+        //if child's return_point is not set, propagate the parent's return_point value
+        if (child->last->return_point==0) {
+            child->last->return_point = parent->last->return_point;
+
+        }
+        parent->last->next = child;
+        child->prev = parent->last;
+        parent->last = child->last;
+        return parent;
+    } else if (!parent) {
+        return child;
+    } else {
+        return parent;
+    }
+}
+
+void link_statement_to_module(statement_t *new_statement) {
+    link_statements(new_statement,statement_root_module[statement_root_module_current]);
+}
+
+void new_statement_module() {
+    statement_root_module_current++;
+}
+
+void return_to_previous_statement_module() {
+    if (statement_root_module_current==0) {
+        return;
+    }
+
+    statement_root_module_current--;
+}
+
+//const char* statement_type_to_string(statement_t *statement);
---
+++ statements.h
@@ -0,0 +1,125 @@
+#ifndef __STATEMENTS_H
+#define __STATEMENTS_H
+
+#include "semantics.h"
+
+//module zero is the main program
+#define MAX_NUM_OF_MODULES 128
+
+struct statement_t;
+
+enum StatementType {
+    ST_If,
+    ST_While,
+    ST_For,
+    ST_Call,
+    ST_Assignment,
+    ST_Case,
+    ST_With,
+    ST_Read,
+    ST_Write,
+    ST_BadStatement
+};
+
+enum AssignmentType {
+    AT_Expression,
+    AT_String
+};
+
+enum IterSpaceType {
+    FT_DownTo,
+    FT_To
+};
+
+struct statement_if_t {
+    struct statement_t *_true;
+    struct statement_t *_false;
+    expr_t *condition;
+};
+
+struct statement_while_t {
+    struct statement_t* loop;
+    expr_t *condition;
+};
+
+struct statement_assignment_t {
+    enum AssignmentType type;
+    var_t *var;
+    union {
+        expr_t *expr;
+        char *string;
+    };
+};
+
+struct statement_for_t {
+    expr_t *from, *to;
+    var_t *var;
+    struct statement_t *loop;
+    enum IterSpaceType type;
+};
+
+struct statement_call_t {
+    //char *id;
+    //param_t *params;
+    //data_t *type;
+    //int size;
+    func_t *subprogram;
+    expr_list_t *expr_params;
+};
+
+struct statement_with_t {
+    struct statement_t *statement;
+    var_t *var;
+};
+
+struct statement_read_t {
+    var_list_t *var_list;
+};
+
+struct statement_write_t {
+    expr_list_t *expr_list;
+};
+
+typedef struct statement_t {
+    enum StatementType type;
+    union {
+        struct statement_if_t _if;
+        struct statement_while_t _while;
+        struct statement_assignment_t _assignment;
+        struct statement_for_t _for;
+        struct statement_call_t _call;
+        struct statement_with_t _with;
+        struct statement_read_t _read;
+        struct statement_write_t _write;
+    };
+    int return_point; //we check this to see if a function always returns a return value
+
+    struct statement_t *next;
+    struct statement_t *prev;
+    struct statement_t *last;
+    struct statement_t *join;
+} statement_t;
+
+extern statement_t *statement_root_module[MAX_NUM_OF_MODULES];
+extern int statement_root_module_current;
+
+void init_statements();
+
+statement_t *statement_if(expr_t *condition, statement_t *_true, statement_t *_false);
+statement_t *statement_while(expr_t *condition, statement_t *loop);
+statement_t *statement_assignment(var_t *var, expr_t *expr);
+//statement_t *statement_assignment_str(var_t *var, char *string);
+statement_t *statement_for(char *id, iter_t *iter_space, statement_t *loop);
+statement_t *statement_call(char *id, expr_list_t *expr_params);
+statement_t *statement_with(var_t *var, statement_t *statement);
+statement_t *statement_read(var_list_t *var_list);
+statement_t *statement_write(expr_list_t *expr_list);
+
+statement_t *link_statements(statement_t *child, statement_t *parent);
+void link_statement_to_module(statement_t *new_statement);
+void new_statement_module();
+void return_to_previous_statement_module();
+
+const char* statement_type_to_string(statement_t *statement);
+
+#endif // __STATEMENTS_H
