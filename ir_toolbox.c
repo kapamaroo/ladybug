06325a4c3a1ea56b6c2a9331edb809239fc72a48
Maroudas Manolis <kapamaroo@gmail.com>
2012-01-14 20:06:09 +0200

    fixes
    reorganize calculate_lvalue()
    clean up expr to ir functions

--- ir_toolbox.c
+++ ir_toolbox.c
@@ -13,28 +13,6 @@
 #include "err_buff.h"
 #include "reg.h"
 
-ir_node_t *ir_move_reg(reg_t *reg) {
-    ir_node_t *arch_node;
-    ir_node_t *arch_node2;
-    ir_node_t *move_node;
-
-    arch_node = new_ir_node_t(NODE_RVAL_ARCH);
-    arch_node->reg = &R_zero;
-
-    arch_node2 = new_ir_node_t(NODE_RVAL_ARCH);
-    arch_node2->reg = reg;
-
-    move_node = new_ir_node_t(NODE_RVAL);
-    move_node->op_rval = OP_PLUS;
-    move_node->ir_rval = arch_node;
-    move_node->ir_rval2 = arch_node2;
-
-    //consider integer
-    move_node->data_is = TYPE_INT;
-
-    return move_node;
-}
-
 op_t op_invert_cond(op_t op) {
     //invert the check
 
@@ -211,15 +189,9 @@ ir_node_t *expr_tree_to_ir_tree(expr_t *ltree) {
             case OP_PLUS:
             case OP_MINUS:
             case OP_MULT:
-                tmp_node = ir_move_reg(&R_lo);
-                break;
             case OP_RDIV:
-                break;
             case OP_DIV:
-                tmp_node = ir_move_reg(&R_lo);
-                break;
             case OP_MOD:
-                tmp_node = ir_move_reg(&R_hi);
                 break;
             case OP_AND:
             case OP_OR:
@@ -271,16 +243,16 @@ ir_node_t *expr_tree_to_ir_tree(expr_t *ltree) {
         }
     }
     else if (ltree->expr_is==EXPR_HARDCODED_CONST) {
-        if (ltree->ival==0 || ltree->fval==0 || ltree->cval==0) {
-            new_node = new_ir_node_t(NODE_RVAL_ARCH);
-            new_node->reg = &R_zero;
-        } else {
-            new_node = new_ir_node_t(NODE_HARDCODED_RVAL);
-            new_node->ival = ltree->ival;
-            new_node->fval = ltree->fval;
-            new_node->cval = ltree->cval;
-            new_node->data_is = ltree->datatype->is;
-        }
+        //if (ltree->ival==0 || ltree->fval==0 || ltree->cval==0) {
+        //    new_node = new_ir_node_t(NODE_RVAL);
+        //    new_node->reg = &R_zero;
+        //} else {
+        new_node = new_ir_node_t(NODE_HARDCODED_RVAL);
+        new_node->ival = ltree->ival;
+        new_node->fval = ltree->fval;
+        new_node->cval = ltree->cval;
+        new_node->data_is = ltree->datatype->is;
+            //}
         return new_node;
     }
     else if (ltree->expr_is==EXPR_NULL_SET) {
@@ -306,7 +278,7 @@ ir_node_t *expr_tree_to_ir_tree(expr_t *ltree) {
         //load the lvalue of every datatype
 
         new_node = new_ir_node_t(NODE_LOAD);
-        new_node->address = calculate_lvalue(ltree->var);
+        new_node->ir_lval = calculate_lvalue(ltree->var);
 
         if (ltree->var->id_is==ID_RETURN) {
             new_func_call = prepare_stack_and_call(ltree->var->scope->scope_owner,ltree->expr_list);
@@ -338,58 +310,59 @@ ir_node_t *expr_tree_to_ir_tree(expr_t *ltree) {
 
 ir_node_t *calculate_lvalue(var_t *v) {
     ir_node_t *new_node;
-    ir_node_t *ptr_deref;
+    ir_node_t *base_address;
 
-    expr_t *address_expr;
+    expr_t *offset_expr;
 
     if (!v) {
         die("UNEXPECTED_ERROR: 74-1");
     }
 
+    base_address = new_ir_node_t(NODE_RVAL_ARCH);
+    if (v->Lvalue->segment==MEM_GLOBAL) {
+        base_address->reg = &R_gp;
+    } else {
+        base_address->reg = &R_sp;
+    }
+
     //calculate the reference address
     if (v->Lvalue->content_type==PASS_REF) {
-        new_node = new_ir_node_t(NODE_HARDCODED_LVAL);
-        new_node->lval = v->Lvalue;
-        new_node->ival = v->Lvalue->seg_offset->ival;
-
-        if (v->Lvalue->segment==MEM_GLOBAL) {
-            new_node->reg = &R_gp;
-        } else {
-            new_node->reg = &R_sp;
-        }
+        new_node = new_ir_node_t(NODE_LVAL);
 
         //set the address so far
-        new_node->address = expr_tree_to_ir_tree(v->Lvalue->seg_offset);
+        new_node->address = base_address;
+        new_node->offset = expr_tree_to_ir_tree(v->Lvalue->seg_offset);
 
         //load the final address
-        ptr_deref = new_ir_node_t(NODE_LOAD);
-        ptr_deref->address = new_node;
+        base_address = new_ir_node_t(NODE_LOAD);
+        base_address->ir_lval = new_node;
 
-        //put the offset separate after the load
-        new_node = new_ir_node_t(NODE_LVAL);
-        new_node->address = ptr_deref;
-        new_node->offset = expr_tree_to_ir_tree(v->Lvalue->offset_expr);
-    } else { //PASS_VAL
-        if (v->Lvalue->offset_expr->expr_is==EXPR_HARDCODED_CONST) {
-            new_node = new_ir_node_t(NODE_HARDCODED_LVAL);
-            new_node->lval = v->Lvalue;
-            new_node->ival = v->Lvalue->seg_offset->ival + v->Lvalue->offset_expr->ival;
-
-            if (v->Lvalue->segment==MEM_GLOBAL) {
-                new_node->reg = &R_gp;
-            } else {
-                new_node->reg = &R_sp;
-            }
-
-            //put the offset together with the *address
-            address_expr = expr_relop_equ_addop_mult(v->Lvalue->seg_offset,OP_PLUS,v->Lvalue->offset_expr);
-            new_node->address = expr_tree_to_ir_tree(address_expr);
-        } else {
-            new_node = new_ir_node_t(NODE_LVAL);
-            new_node->address = expr_tree_to_ir_tree(v->Lvalue->seg_offset);
-            new_node->offset = expr_tree_to_ir_tree(v->Lvalue->offset_expr);
-        }
+        offset_expr = expr_from_hardcoded_int(0);
+    } else {
+        offset_expr = v->Lvalue->seg_offset;
     }
+
+    if (v->Lvalue->offset_expr->expr_is==EXPR_HARDCODED_CONST) {
+        new_node = new_ir_node_t(NODE_LVAL);
+
+        offset_expr = expr_relop_equ_addop_mult(offset_expr,OP_PLUS,v->Lvalue->offset_expr);
+
+        new_node->address = base_address;
+        new_node->offset = expr_tree_to_ir_tree(offset_expr);
+    } else {
+        new_node = base_address;
+        base_address = new_ir_node_t(NODE_RVAL);
+        base_address->op_rval = OP_PLUS;
+        base_address->ir_rval = new_node;
+        base_address->ir_rval2 = expr_tree_to_ir_tree(v->Lvalue->offset_expr);
+
+        new_node = new_ir_node_t(NODE_LVAL);
+        new_node->address = base_address;
+        new_node->offset = expr_tree_to_ir_tree(offset_expr);
+    }
+
+    new_node->data_is = v->datatype->is;
+
     return new_node;
 }
 
@@ -469,11 +442,10 @@ ir_node_t *prepare_stack_and_call(func_t *subprogram, expr_list_t *list) {
             tmp_var->Lvalue->content_type = PASS_VAL;
 
             //reminder: we are talking about stack Lvalues
-            //there are no offsets, parameters do not change position in stack
-            //this means that a stack address is HARDCODED_LVAL
+            //parameters do not change position in stack
 
             tmp_assign = new_ir_node_t(NODE_ASSIGN);
-            tmp_assign->address = calculate_lvalue(tmp_var);
+            tmp_assign->ir_lval = calculate_lvalue(tmp_var);
             tmp_assign->ir_rval = calculate_lvalue(list->expr_list[i]->var);
 
             //convert to RVAL
