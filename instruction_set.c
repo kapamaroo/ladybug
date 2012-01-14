a11721d8110390271b8af33344d660907c201a6c
Maroudas Manolis <kapamaroo@gmail.com>
2012-01-14 19:05:24 +0200

    add mips instruction set

---
+++ instruction_set.c
@@ -0,0 +1,169 @@
+#include "instruction_set.h"
+
+//arithmetic
+mips_instr_t I_add   = {.name="add",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_addi  = {.name="addi",  .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+mips_instr_t I_addiu = {.name="addiu", .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+mips_instr_t I_addu  = {.name="addu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_lui   = {.name="lui",   .fmt=FMT_RD_IMM,        .type=INSTR_ISA};
+mips_instr_t I_sub   = {.name="sub",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_subu  = {.name="subu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_abs   = {.name="abs",   .fmt=FMT_RD_RS,         .type=INSTR_ISA};
+
+mips_instr_t I_la    = {.name="la",    .fmt=FMT_RD_LABEL,      .type=INSTR_PSEUDO_ISA};   //load address, expand to lui, ori
+mips_instr_t I_move  = {.name="move",  .fmt=FMT_RD_RS,         .type=INSTR_PSEUDO_ISA};   //convert to add
+mips_instr_t I_clear = {.name="clear", .fmt=FMT_RD,            .type=INSTR_PSEUDO_ISA};   //convert to add
+mips_instr_t I_li    = {.name="li",    .fmt=FMT_RD_IMM32,      .type=INSTR_PSEUDO_ISA};   //load 32bit imm, expand to lui, ori
+mips_instr_t I_not   = {.name="not",   .fmt=FMT_RD_RS,         .type=INSTR_PSEUDO_ISA};   //convert to nor $rd, $rs, $rs
+mips_instr_t I_neg   = {.name="neg",   .fmt=FMT_RD_RS,         .type=INSTR_PSEUDO_ISA};   //convert to sub
+mips_instr_t I_negu  = {.name="negu",  .fmt=FMT_RD_RS,         .type=INSTR_PSEUDO_ISA};   //convert to subu
+mips_instr_t I_nop   = {.name="nop",   .fmt=FMT_EMPTY,         .type=INSTR_PSEUDO_ISA};
+
+mips_instr_t I_seb = {.name="seb",     .fmt=FMT_RD_RS,         .type=INSTR_ISA};          //sign extend of byte in register
+mips_instr_t I_seh = {.name="seh",     .fmt=FMT_RD_RS,         .type=INSTR_ISA};          //sign extend of halfword in register
+mips_instr_t I_clo = {.name="clo",     .fmt=FMT_RD_RS,         .type=INSTR_ISA};          //count leading ones
+mips_instr_t I_clz = {.name="clz",     .fmt=FMT_RD_RS,         .type=INSTR_ISA};          //count leading zeroes
+
+//shift
+mips_instr_t I_sll  = {.name="sll",    .fmt=FMT_RD_RS_SHIFT,   .type=INSTR_ISA};
+mips_instr_t I_sllv = {.name="sllv",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_sra  = {.name="sra",    .fmt=FMT_RD_RS_SHIFT,   .type=INSTR_ISA};
+mips_instr_t I_srav = {.name="srav",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_srl  = {.name="srl",    .fmt=FMT_RD_RS_SHIFT,   .type=INSTR_ISA};
+mips_instr_t I_srlv = {.name="srlv",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+
+//rotate
+mips_instr_t I_rotr  = {.name="rotr",  .fmt=FMT_RD_RS_SHIFT,   .type=INSTR_ISA};
+mips_instr_t I_rotrv = {.name="rotrv", .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+
+//logical
+mips_instr_t I_and  = {.name="and",    .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_andi = {.name="andi",   .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+mips_instr_t I_nor  = {.name="nor",    .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_or   = {.name="or",     .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_ori  = {.name="ori",    .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+mips_instr_t I_xor  = {.name="xor",    .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_xori = {.name="xori",   .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+
+mips_instr_t I_ext  = {.name="ext",    .fmt=FMT_RD_RS_IMM_IMM, .type=INSTR_ISA};          //extract bits
+mips_instr_t I_ins  = {.name="ins",    .fmt=FMT_RD_RS_IMM_IMM, .type=INSTR_ISA};          //insert bits
+mips_instr_t I_wsbh = {.name="wsbh",   .fmt=FMT_RD_RS,         .type=INSTR_ISA};          //byte swap within halfwords
+
+//condition test and set (NOT atomic)
+mips_instr_t I_slt   = {.name="slt",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_slti  = {.name="slti",  .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+mips_instr_t I_sltiu = {.name="sltiu", .fmt=FMT_RD_RS_IMM,     .type=INSTR_ISA};
+mips_instr_t I_sltu  = {.name="sltu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};
+mips_instr_t I_seq   = {.name="seq",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sne   = {.name="sne",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sge   = {.name="sge",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sgeu  = {.name="sgeu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sgt   = {.name="sgt",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sgtu  = {.name="sgtu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sle   = {.name="sle",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_sleu  = {.name="sleu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+
+//condition test and move
+mips_instr_t I_movn = {.name="movn",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};          //mips 4
+mips_instr_t I_movz = {.name="movz",   .fmt=FMT_RD_RS_RT,      .type=INSTR_ISA};          //mips 4
+
+//muldiv
+mips_instr_t I_div   = {.name="div",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_divu  = {.name="divu",  .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_mult  = {.name="mult",  .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_multu = {.name="multu", .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_rem   = {.name="rem",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};   //expand to use mfhi (remainder)
+mips_instr_t I_remu  = {.name="rem",   .fmt=FMT_RD_RS_RT,      .type=INSTR_PSEUDO_ISA};   //expand to use mfhi (remainder)
+
+mips_instr_t I_madd  = {.name="madd",  .fmt=FMT_RS_RT,         .type=INSTR_ISA};          //mips 4
+mips_instr_t I_maddu = {.name="maddu", .fmt=FMT_RS_RT,         .type=INSTR_ISA};          //mips 4
+mips_instr_t I_msub  = {.name="msub",  .fmt=FMT_RS_RT,         .type=INSTR_ISA};          //mips 4
+mips_instr_t I_msubu = {.name="msubu", .fmt=FMT_RS_RT,         .type=INSTR_ISA};          //mips 4
+
+//acc
+mips_instr_t I_mfhi = {.name="mfhi",   .fmt=FMT_RD,            .type=INSTR_ISA};
+mips_instr_t I_mflo = {.name="mhlo",   .fmt=FMT_RD,            .type=INSTR_ISA};
+mips_instr_t I_mthi = {.name="mthi",   .fmt=FMT_RS,            .type=INSTR_ISA};
+mips_instr_t I_mtlo = {.name="mtlo",   .fmt=FMT_RS,            .type=INSTR_ISA};
+
+//branch
+mips_instr_t I_beq  = {.name="beq",    .fmt=FMT_RS_RT_OFFSET,  .type=INSTR_ISA};
+mips_instr_t I_bne  = {.name="bne",    .fmt=FMT_RS_RT_OFFSET,  .type=INSTR_ISA};
+
+mips_instr_t I_bgez = {.name="bgez",   .fmt=FMT_RS_OFFSET,     .type=INSTR_ISA};
+mips_instr_t I_bgtz = {.name="bgtz",   .fmt=FMT_RS_OFFSET,     .type=INSTR_ISA};
+mips_instr_t I_blez = {.name="blez",   .fmt=FMT_RS_OFFSET,     .type=INSTR_ISA};
+mips_instr_t I_bltz = {.name="bltz",   .fmt=FMT_RS_OFFSET,     .type=INSTR_ISA};
+
+mips_instr_t I_bgezal = {.name="bgezal", .fmt=FMT_RS_OFFSET,   .type=INSTR_ISA};
+mips_instr_t I_bltzal = {.name="bltzal", .fmt=FMT_RS_OFFSET,   .type=INSTR_ISA};
+
+mips_instr_t I_bc1t = {.name="bc1t",     .fmt=FMT_OFFSET,      .type=INSTR_ISA};          //test (cc=0) control bit (cc==1)
+mips_instr_t I_bc1f = {.name="bc1f",     .fmt=FMT_OFFSET,      .type=INSTR_ISA};          //test (cc=0) control bit (cc==0)
+
+mips_instr_t I_b    = {.name="b",      .fmt=FMT_OFFSET,        .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_bal  = {.name="bal",    .fmt=FMT_OFFSET,        .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_beqz = {.name="beqz",   .fmt=FMT_RS_OFFSET,     .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_bnez = {.name="bnez",   .fmt=FMT_RS_OFFSET,     .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_bgt  = {.name="bgt",    .fmt=FMT_RS_RT_OFFSET,  .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_blt  = {.name="blt",    .fmt=FMT_RS_RT_OFFSET,  .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_bge  = {.name="bge",    .fmt=FMT_RS_RT_OFFSET,  .type=INSTR_PSEUDO_ISA};
+mips_instr_t I_ble  = {.name="ble",    .fmt=FMT_RS_RT_OFFSET,  .type=INSTR_PSEUDO_ISA};
+
+//jumps
+mips_instr_t I_j    = {.name="j",      .fmt=FMT_ADDR,          .type=INSTR_ISA};
+mips_instr_t I_jal  = {.name="jal",    .fmt=FMT_ADDR,          .type=INSTR_ISA};
+mips_instr_t I_jalr = {.name="jalr",   .fmt=FMT_RD_RS,         .type=INSTR_ISA};
+mips_instr_t I_jr   = {.name="jr",     .fmt=FMT_RS,            .type=INSTR_ISA};
+
+//load
+mips_instr_t I_lb   = {.name="lb",    .fmt=FMT_LOAD,           .type=INSTR_ISA};
+mips_instr_t I_lbu  = {.name="lbu",   .fmt=FMT_LOAD,           .type=INSTR_ISA};
+mips_instr_t I_lh   = {.name="lh",    .fmt=FMT_LOAD,           .type=INSTR_ISA};
+mips_instr_t I_lhu  = {.name="lhu",   .fmt=FMT_LOAD,           .type=INSTR_ISA};
+mips_instr_t I_lw   = {.name="lw",    .fmt=FMT_LOAD,           .type=INSTR_ISA};
+mips_instr_t I_lwc1 = {.name="lwc1",  .fmt=FMT_LOAD,           .type=INSTR_ISA};
+mips_instr_t I_lwl  = {.name="lwl",   .fmt=FMT_LOAD,           .type=INSTR_ISA};         //load (for unaligned data)
+mips_instr_t I_lwr  = {.name="lwr",   .fmt=FMT_LOAD,           .type=INSTR_ISA};         //load (for unaligned data)
+mips_instr_t I_ulw  = {.name="ulw",   .fmt=FMT_LOAD,           .type=INSTR_PSEUDO_ISA};  //load unaligned data
+mips_instr_t I_ll = {.name="ll",      .fmt=FMT_LOAD,           .type=INSTR_ISA};         //load atomic (mips 2)
+//store
+mips_instr_t I_sb   = {.name="sb",    .fmt=FMT_STORE,          .type=INSTR_ISA};
+mips_instr_t I_sh   = {.name="sh",    .fmt=FMT_STORE,          .type=INSTR_ISA};
+mips_instr_t I_sw   = {.name="sw",    .fmt=FMT_STORE,          .type=INSTR_ISA};
+mips_instr_t I_swc1 = {.name="swc1",  .fmt=FMT_STORE,          .type=INSTR_ISA};
+mips_instr_t I_swl  = {.name="swl",   .fmt=FMT_STORE,          .type=INSTR_ISA};         //store (for unaligned data)
+mips_instr_t I_swr  = {.name="swr",   .fmt=FMT_STORE,          .type=INSTR_ISA};         //store (for unaligned data)
+mips_instr_t I_usw  = {.name="usw",   .fmt=FMT_STORE,          .type=INSTR_PSEUDO_ISA};  //store unaligned data
+mips_instr_t I_sc = {.name="sc",      .fmt=FMT_STORE,          .type=INSTR_ISA};         //store atomic (mips 2)
+
+//float single arithmetic
+mips_instr_t I_add_s = {.name="add.s",   .fmt=FMT_RD_RS_RT,    .type=INSTR_ISA};
+mips_instr_t I_sub_s = {.name="sub.s",   .fmt=FMT_RD_RS_RT,    .type=INSTR_ISA};
+mips_instr_t I_mul_s = {.name="mul.s",   .fmt=FMT_RD_RS_RT,    .type=INSTR_ISA};
+mips_instr_t I_div_s = {.name="div.s",   .fmt=FMT_RD_RS_RT,    .type=INSTR_ISA};
+mips_instr_t I_abs_s = {.name="abs.s",   .fmt=FMT_RD_RS,       .type=INSTR_ISA};
+mips_instr_t I_neg_s = {.name="neg.s",   .fmt=FMT_RD_RS,       .type=INSTR_ISA};
+mips_instr_t I_mov_s = {.name="mov.s",   .fmt=FMT_RD_RS,       .type=INSTR_ISA};    //move fpr to fpr
+
+mips_instr_t I_c_eq_s = {.name="c.eq.s", .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //eq with bc1t, neq wth bc1f
+mips_instr_t I_c_le_s = {.name="c.le.s", .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //le with bc1t, gt wth bc1f
+mips_instr_t I_c_lt_s = {.name="c.lt.s", .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //lt with bc1t, ge wth bc1f
+
+//mips_instr_t I_c_nlt_s = {.name="c.ge.s", .fmt=FMT_RS_RT,       .type=INSTR_ISA};
+//mips_instr_t I_c_nle_s = {.name="c.gt.s", .fmt=FMT_RS_RT,       .type=INSTR_ISA};
+//mips_instr_t I_c_neq_s = {.name="c.neq.s", .fmt=FMT_RS_RT,       .type=INSTR_ISA};
+
+//other c1
+mips_instr_t I_mtc1 = {.name="mtc1",     .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //move gpr to fpr
+mips_instr_t I_mfc1 = {.name="mfc1",     .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //move fpr to gpr
+mips_instr_t I_ctc1 = {.name="ctc1",     .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //move gpr to fcr
+mips_instr_t I_cfc1 = {.name="cfc1",     .fmt=FMT_RS_RT,       .type=INSTR_ISA};    //move fcr to gpr
+
+//syscall
+mips_instr_t I_syscall = {.name="syscall", .fmt=FMT_EMPTY,     .type=INSTR_ISA};
+mips_instr_t I_break   = {.name="break",   .fmt=FMT_EMPTY,     .type=INSTR_ISA};
+
+//convert
+mips_instr_t I_cvt_w_s = {.name="cvt.w.s", .fmt=FMT_RD_RS,     .type=INSTR_ISA};
+mips_instr_t I_cvt_s_w = {.name="cvt.s.w", .fmt=FMT_RD_RS,     .type=INSTR_ISA};
---
+++ instruction_set.h
@@ -0,0 +1,231 @@
+#ifndef _INSTRUCTION_SET
+#define _INSTRUCTION_SET
+
+#include "mips32.h"
+#include "reg.h"
+
+enum instr_type_t {
+    INSTR_ISA,
+    INSTR_PSEUDO_ISA
+};
+
+enum instr_format {
+    FMT_RD_RS_RT,
+    FMT_RD_RS,
+    FMT_RS_RT,
+    FMT_RD_RS_IMM_IMM,  // 5-bit each
+    FMT_RD,
+    FMT_RD_IMM,         //16-bit imm
+    FMT_RD_IMM32,       //32-bit imm
+    FMT_RD_LABEL,       //32-bit address
+    FMT_RS,
+    FMT_RD_RS_SHIFT,    // 5-bit imm
+    FMT_RD_RS_IMM,      //16-bit imm
+    FMT_LOAD,
+    FMT_STORE,
+    FMT_RS_RT_OFFSET,   //18-bit offset  (branch)
+    FMT_RS_OFFSET,      //18-bit offset  (branch)
+    FMT_ADDR,           //28-bit address (jump)
+    FMT_OFFSET,         //18-bit offset  (branch)
+    FMT_EMPTY
+};
+
+typedef struct mips_instr_t {
+    enum instr_type_t type;
+    enum instr_format fmt;
+    char *name;
+} mips_instr_t;
+
+typedef struct instr_t {
+    mips_instr_t *mips_instr;
+
+    char *label;
+    reg_t *Rd;
+    reg_t *Rs;
+    reg_t *Rt;
+
+    unsigned long virt_Rd; //use virtual registers if we have not allocated registers
+    unsigned long virt_Rs;
+    unsigned long virt_Rt;
+
+    int ival; //possible offset, address, immediate
+    char *goto_label;
+
+    struct instr_t *prev;
+    struct instr_t *next;
+    struct instr_t *last;
+} instr_t;
+
+/* Instruction set */
+
+//arithmetic
+extern mips_instr_t I_add;
+extern mips_instr_t I_addi;
+extern mips_instr_t I_addiu;
+extern mips_instr_t I_addu;
+extern mips_instr_t I_lui;
+extern mips_instr_t I_sub;
+extern mips_instr_t I_subu;
+extern mips_instr_t I_abs;
+
+extern mips_instr_t I_la;
+extern mips_instr_t I_move;
+extern mips_instr_t I_clear;
+extern mips_instr_t I_li;
+extern mips_instr_t I_not;
+extern mips_instr_t I_neg;
+extern mips_instr_t I_negu;
+extern mips_instr_t I_nop;
+
+extern mips_instr_t I_seb;
+extern mips_instr_t I_seh;
+extern mips_instr_t I_clo;
+extern mips_instr_t I_clz;
+
+//shift
+extern mips_instr_t I_sll;
+extern mips_instr_t I_sllv;
+extern mips_instr_t I_sra;
+extern mips_instr_t I_srav;
+extern mips_instr_t I_srl;
+extern mips_instr_t I_srlv;
+
+//rotate
+extern mips_instr_t I_rotr;
+extern mips_instr_t I_rotrv;
+
+//logical
+extern mips_instr_t I_and;
+extern mips_instr_t I_andi;
+extern mips_instr_t I_nor;
+extern mips_instr_t I_or;
+extern mips_instr_t I_ori;
+extern mips_instr_t I_xor;
+extern mips_instr_t I_xori;
+
+extern mips_instr_t I_ext;
+extern mips_instr_t I_ins;
+extern mips_instr_t I_wsbh;
+
+//condition test and set (NOT atomic)
+extern mips_instr_t I_slt;
+extern mips_instr_t I_slti;
+extern mips_instr_t I_sltiu;
+extern mips_instr_t I_sltu;
+extern mips_instr_t I_seq;
+extern mips_instr_t I_sne;
+extern mips_instr_t I_sge;
+extern mips_instr_t I_sgeu;
+extern mips_instr_t I_sgt;
+extern mips_instr_t I_sgtu;
+extern mips_instr_t I_sle;
+extern mips_instr_t I_sleu;
+
+//condition test and move
+extern mips_instr_t I_movn;
+extern mips_instr_t I_movz;
+
+//muldiv
+extern mips_instr_t I_div;
+extern mips_instr_t I_divu;
+extern mips_instr_t I_mult;
+extern mips_instr_t I_multu;
+extern mips_instr_t I_rem;
+extern mips_instr_t I_remu;
+
+extern mips_instr_t I_madd;
+extern mips_instr_t I_maddu;
+extern mips_instr_t I_msub;
+extern mips_instr_t I_msubu;
+
+//acc
+extern mips_instr_t I_mfhi;
+extern mips_instr_t I_mflo;
+extern mips_instr_t I_mthi;
+extern mips_instr_t I_mtlo;
+
+//branch
+extern mips_instr_t I_beq;
+extern mips_instr_t I_bne;
+
+extern mips_instr_t I_bgez;
+extern mips_instr_t I_bgtz;
+extern mips_instr_t I_blez;
+extern mips_instr_t I_bltz;
+
+extern mips_instr_t I_bgezal;
+extern mips_instr_t I_bltzal;
+
+extern mips_instr_t I_b;
+extern mips_instr_t I_bal;
+extern mips_instr_t I_beqz;
+extern mips_instr_t I_bnez;
+extern mips_instr_t I_bgt;
+extern mips_instr_t I_blt;
+extern mips_instr_t I_bge;
+extern mips_instr_t I_ble;
+
+//jumps
+extern mips_instr_t I_j;
+extern mips_instr_t I_jal;
+extern mips_instr_t I_jalr;
+extern mips_instr_t I_jr;
+
+//load
+extern mips_instr_t I_lb;
+extern mips_instr_t I_lbu;
+extern mips_instr_t I_lh;
+extern mips_instr_t I_lhu;
+extern mips_instr_t I_lw;
+extern mips_instr_t I_lwc1;
+extern mips_instr_t I_lwl;
+extern mips_instr_t I_lwr;
+extern mips_instr_t I_l_s;
+extern mips_instr_t I_ulw;
+
+//store
+extern mips_instr_t I_sb;
+extern mips_instr_t I_sh;
+extern mips_instr_t I_sw;
+extern mips_instr_t I_swc1;
+extern mips_instr_t I_swl;
+extern mips_instr_t I_swr;
+extern mips_instr_t I_s_s;
+extern mips_instr_t I_usw;
+
+//atomic
+extern mips_instr_t I_ll;
+extern mips_instr_t I_sc;
+
+//syscall
+extern mips_instr_t I_syscall;
+extern mips_instr_t I_break;
+
+//float single arithmetic
+extern mips_instr_t I_add_s;
+extern mips_instr_t I_sub_s;
+extern mips_instr_t I_mul_s;
+extern mips_instr_t I_div_s;
+extern mips_instr_t I_abs_s;
+extern mips_instr_t I_neg_s;
+extern mips_instr_t I_mov_s;
+
+extern mips_instr_t I_c_eq_s;
+extern mips_instr_t I_c_le_s;
+extern mips_instr_t I_c_lt_s;
+
+//convert
+extern mips_instr_t I_cvt_w_s;
+extern mips_instr_t I_cvt_s_w;
+
+//other c1
+extern mips_instr_t I_bc1t;
+extern mips_instr_t I_bc1f;
+
+extern mips_instr_t I_mtc1;
+extern mips_instr_t I_mfc1;
+
+extern mips_instr_t I_ctc1;
+extern mips_instr_t I_cfc1;
+
+#endif
