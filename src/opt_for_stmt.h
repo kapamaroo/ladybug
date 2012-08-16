2029418fc1a2d20310dbd89892b420bf4d9a97dc
Maroudas Manolis <kapamaroo@gmail.com>
2012-08-16 12:58:39 +0300

    add header for loop optimizations
    	new file:   src/opt_for_stmt.h

---
+++ src/opt_for_stmt.h
@@ -0,0 +1,17 @@
+#ifndef _OPT_FOR_STMT_H
+#define _OPT_FOR_STMT_H
+
+#include "statements.h"
+
+enum opt_loop_type {
+    OPT_UNROLL_CLASSIC,
+    OPT_UNROLL_SYMBOLIC,
+    OPT_LOOP_SIMPLIFY,
+};
+
+void opt_for_init();
+
+//optimizations' wrapper, use this instead of direct optimizing
+void optimize_loops(enum opt_loop_type type);
+
+#endif
